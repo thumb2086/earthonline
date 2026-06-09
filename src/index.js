55ca8ce2747f040c9150240f9513a32efcbea4f6
thumb2086 <cpxru83@gmail.com>
2026-06-09 22:49:48 +0800

    fix: remove proxy from Worker, only serve static assets

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/api/') || url.pathname.startsWith('/socket.io/')) {
-      return fetch('https://earthonline.onrender.com' + url.pathname + url.search, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-      });
-    }
     return env.ASSETS.fetch(request);
   },
 };
