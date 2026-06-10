0794e9008e9fd17c7c9adb79a623c66ce08c78a1
thumb2086 <cpxru83@gmail.com>
2026-06-10 19:06:36 +0800

    remove test file

--- test-socket.js
+++
@@ -1,6 +0,0 @@
-const io = require('socket.io-client');
-const s = io('http://localhost:3001/asia');
-s.on('connect', () => console.log('CONNECTED:', s.id));
-s.on('global_stats', (d) => console.log('STATS:', d.activeUsers, d.multiplier, d.globalProduction));
-s.on('connect_error', (e) => console.log('ERR:', e.message));
-setTimeout(() => { console.log('Done waiting'); process.exit(0); }, 8000);
