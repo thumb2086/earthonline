8259d1dcf0c7c7c5bcf481ffbd22f99ac24ccab1
thumb2086 <cpxru83@gmail.com>
2026-06-11 17:41:39 +0800

    v1.13.3a: ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: { node: true, browser: true, es2021: true },
+  extends: ['eslint:recommended'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+    'no-console': 'off',
+  },
+};
